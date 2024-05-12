68d895510b8f114fcb34e1c47ce755e0004aca4d
Hữu Được <nhd.ars@gmail.com>
2024-05-12 12:29:14 +0700

    update ui

--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -95,152 +95,166 @@ function Contribute() {
   };
 
   return (
-    <div className="p-5 relative max-w-7xl w-full mx-auto">
-      <h1 className="text-3xl mb-5 text-blue-600">Tạo Film Simulation</h1>
-      <Button
-        className={clsx(
-          "bg-blue-600 text-white",
-          "hover:bg-blue-700",
-          "focus:ring-2 focus:ring-blue-600",
-          handling && "cursor-not-allowed",
-          handling && "opacity-50",
-          "transition-all duration-200",
-          "fixed  right-5 bottom-5 "
-        )}
-        onClick={handleUpload}
-        handling={handling}
-      >
-        {handling ? "Đang xử lý..." : "Tạo công thức"}
-      </Button>
-      <div className="my-5">
-        <div className="flex w-full items-center space-x-4">
-          <div className="flex-1">
-            <label className="font-semibold my-5">Tên công thức</label>
-            <Input
-              type=""
-              value={recipe.name}
-              onChange={(e) => setRecipe({ ...recipe, name: e.target.value })} // eslint-disable-line
-            />
+    <div>
+      {
+        //  create full screen loading bg-black opcitiy-50 fixed top-0 left-0 right-0 bottom-0 z-50
+        handling && (
+          <div className="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black opacity-70">
+            <h2 className="absolute top-1/2 left-1/2 -translate-x-1/2 text-xl">
+              <span className="text-white animate-pulse">
+                Đang xử lý, bạn vui lòng đợi 1 lát nhé...
+              </span>
+            </h2>
           </div>
-          <div className="my-5 flex-1">
-            <label className="font-semibold my-5">Category</label>
-            <InputComponent
-              options={["sea-side-view", "street", "forest", "random"]}
-              type="select"
-              value={recipe.category}
-              onChange={(val) => {
-                setRecipe({ ...recipe, category: val });
-              }}
-            />
-          </div>
-        </div>
-        <div className="grid md:grid-cols-5 gap-2 md:gap-8 grid-cols-2">
-          <Recipes
-            onChange={(path: string, val: any) => {
-              const newRecipe = { ...recipe };
-              set(newRecipe, path, val);
-              setRecipe(newRecipe);
-            }}
-            recipe={recipe}
-            handling={handling}
-          />
-        </div>
-        <h2>Sample Images</h2>
-        <div {...getRootProps()} className="border-2 border-dashed p-4">
-          <input
-            {...getInputProps()}
-            className={cn(
-              "w-full h-full",
-              "cursor-pointer",
-              "opacity-0",
-              "absolute",
-              "top-0",
-              "left-0"
-            )}
-          />
-          {isDragActive ? (
-            <p>Drop the files here ...</p>
-          ) : (
-            <p>Drag & drop some files here, or click to select files</p>
+        )
+      }
+      <div className="p-5 relative max-w-7xl w-full mx-auto">
+        <h1 className="text-3xl mb-5 text-blue-600">Tạo Film Simulation</h1>
+        <Button
+          className={clsx(
+            "bg-blue-600 text-white",
+            "hover:bg-blue-700",
+            "focus:ring-2 focus:ring-blue-600",
+            handling && "cursor-not-allowed",
+            handling && "opacity-50",
+            "transition-all duration-200",
+            "fixed  right-5 bottom-5 "
           )}
-          {uploading && (
-            <p className="text-blue-600">
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-              Đang tải lên...
-            </p>
-          )}
-        </div>
-        {images.length > 0 && (
-          <div>
-            <h2>Preview</h2>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-              {images.map((file, index) => (
-                <div key={index} className="relative grid gap-4">
-                  <img
-                    key={index}
-                    src={URL.createObjectURL(file)}
-                    alt=""
-                    className="w-full h-auto object-cover rounded-md"
-                  />
-                  <Input
-                    type="text"
-                    value={recipe.images?.[index]?.name}
-                    onBlur={(e) => {
-                      const imageClone = images[index];
-                      const blob = imageClone.slice(
-                        0,
-                        imageClone.size,
-                        "image/jpeg"
-                      );
-                      const newImage = new File([blob], e.target.value, {
-                        type: imageClone.type,
-                      });
-                      const newImages = cloneDeep(images);
-                      newImages[index] = newImage;
-                      setImages(newImages);
-                    }}
-                    placeholder="Tên ảnh"
-                    className="w-full"
-                  />
-
-                  <Button
-                    size={"icon"}
-                    variant={"default"}
-                    color="red"
-                    className="absolute top-3 right-3"
-                    onClick={() => {
-                      const newImages = images.filter((_, i) => i !== index);
-                      setImages(newImages);
-                    }}
-                  >
-                    <span>
-                      <XIcon />
-                    </span>
-                  </Button>
-                </div>
-              ))}
+          onClick={handleUpload}
+          handling={handling}
+        >
+          {handling ? "Đang xử lý..." : "Tạo công thức"}
+        </Button>
+        <div className="my-5">
+          <div className="flex w-full items-center space-x-4">
+            <div className="flex-1">
+              <label className="font-semibold my-5">Tên công thức</label>
+              <Input
+                type=""
+                value={recipe.name}
+                onChange={(e) => setRecipe({ ...recipe, name: e.target.value })} // eslint-disable-line
+              />
+            </div>
+            <div className="my-5 flex-1">
+              <label className="font-semibold my-5">Category</label>
+              <InputComponent
+                options={["sea-side-view", "street", "forest", "random"]}
+                type="select"
+                value={recipe.category}
+                onChange={(val) => {
+                  setRecipe({ ...recipe, category: val });
+                }}
+              />
             </div>
           </div>
-        )}
+          <div className="grid md:grid-cols-5 gap-2 md:gap-8 grid-cols-2">
+            <Recipes
+              onChange={(path: string, val: any) => {
+                const newRecipe = { ...recipe };
+                set(newRecipe, path, val);
+                setRecipe(newRecipe);
+              }}
+              recipe={recipe}
+              handling={handling}
+            />
+          </div>
+          <h2>Sample Images</h2>
+          <div {...getRootProps()} className="border-2 border-dashed p-4">
+            <input
+              {...getInputProps()}
+              className={cn(
+                "w-full h-full",
+                "cursor-pointer",
+                "opacity-0",
+                "absolute",
+                "top-0",
+                "left-0"
+              )}
+            />
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p>Drag & drop some files here, or click to select files</p>
+            )}
+            {uploading && (
+              <p className="text-blue-600">
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Đang tải lên...
+              </p>
+            )}
+          </div>
+          {images.length > 0 && (
+            <div>
+              <h2>Preview</h2>
+              <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                {images.map((file, index) => (
+                  <div key={index} className="relative grid gap-4">
+                    <img
+                      key={index}
+                      src={URL.createObjectURL(file)}
+                      alt=""
+                      className="w-full h-auto object-cover rounded-md"
+                    />
+                    <Input
+                      type="text"
+                      value={recipe.images?.[index]?.name}
+                      onBlur={(e) => {
+                        const imageClone = images[index];
+                        const blob = imageClone.slice(
+                          0,
+                          imageClone.size,
+                          "image/jpeg"
+                        );
+                        const newImage = new File([blob], e.target.value, {
+                          type: imageClone.type,
+                        });
+                        const newImages = cloneDeep(images);
+                        newImages[index] = newImage;
+                        setImages(newImages);
+                      }}
+                      placeholder="Tên ảnh"
+                      className="w-full"
+                    />
+
+                    <Button
+                      size={"icon"}
+                      variant={"default"}
+                      color="red"
+                      className="absolute top-3 right-3"
+                      onClick={() => {
+                        const newImages = images.filter((_, i) => i !== index);
+                        setImages(newImages);
+                      }}
+                    >
+                      <span>
+                        <XIcon />
+                      </span>
+                    </Button>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
